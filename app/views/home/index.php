011675c35b27c84ebbfe60170e0ce902055c07f5
Gayangi Seneviratne <sgayangi@gmail.com>
2020-10-03 20:56:35 +0530

    capital letters updates

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -10,12 +10,12 @@
 
 	<?php if (currentUser()) {
   
-    echo "<h1 style=\"margin-top:15px\"> Welcome Back</h1><div>";
+    echo "<h1 style=\"margin-top:15px\"> Welcome back</h1><div>";
     echo "<h2 style=\" color:#FEA736; \">";
     echo currentUser()->username;
     echo "</h2></div>";
   }else {
-  echo "<h1 style=\"margin-top:15px\"> Welcome to the <div>Website </div></h1>";
+  echo "<h1 style=\"margin-top:15px\"> Welcome to the <div>website </div></h1>";
   }
 
   ?>
